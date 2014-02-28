4bd68d4335e556063921731100335c5dbe7b3703
Brian Holland <neumaics@gmail.com>
2014-02-27 20:09:50 -0700

    Addition of simple JavaFx application, plugin. Enhancement of .gitignore.

--- src/main/java/com/neumaics/simpleconsole/SimpleConsole.java
+++ src/main/java/com/neumaics/simpleconsole/SimpleConsole.java
@@ -1,5 +1,30 @@
 package com.neumaics.simpleconsole;
 
-public class SimpleConsole {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+/**
+ * 
+ * @author neumaics
+ *
+ */
+public class SimpleConsole extends Application{
+
+	public static void main(String[] args) {
+        launch(args);
+    }
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("SimpleConsoleDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("style.css");
+        
+        stage.setScene(scene);
+        stage.show();
+	}
 }
---
+++ src/main/java/com/neumaics/simpleconsole/controllers/SimpleConsoleController.java
@@ -0,0 +1,13 @@
+package com.neumaics.simpleconsole.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SimpleConsoleController implements Initializable {
+	
+	public void initialize(URL url, ResourceBundle rb) {
+    	
+    }
+}
