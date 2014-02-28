f95da41b3e7a1797f4b7b2b3002d48d1c0150c3b
Brian Holland <neumaics@gmail.com>
2014-02-27 20:45:45 -0700

    Rename of the stylesheet, more fleshing out of the UI.

--- src/main/java/com/neumaics/simpleconsole/controllers/SimpleConsoleController.java
+++ src/main/java/com/neumaics/simpleconsole/controllers/SimpleConsoleController.java
@@ -3,9 +3,18 @@ package com.neumaics.simpleconsole.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
 
 public class SimpleConsoleController implements Initializable {
+	
+	@FXML
+	TextArea workPad;
+	
+	@FXML
+	TextArea outputPad;
 	
 	public void initialize(URL url, ResourceBundle rb) {
     	
